e5d1f2b3a539c82d66ae116b8fd9fdcbc6838b5f
Thiago Mota <th.araujomota@gmail.com>
2023-02-26 22:28:24 -0300

    feat: adds get_unique_job_types function

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -14,21 +14,11 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    jobs = read(path)
+    job_types = set()
+    for job in jobs:
+        job_types.add(job["job_type"])
+    return job_types
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
