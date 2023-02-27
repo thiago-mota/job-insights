abdb376fe6c759278af28cadf655be99cb443027
Thiago Mota <th.araujomota@gmail.com>
2023-02-26 23:08:52 -0300

    feat: adds get_unique_industries function

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,12 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    jobs = read(path)
+    industries = set()
+    for industry in jobs:
+        if industry["industry"] != "":
+            industries.add(industry["industry"])
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
