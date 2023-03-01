81e27b41a6cdd84569023cd7b716147b4b4120f4
Thiago Mota <th.araujomota@gmail.com>
2023-03-01 00:08:35 -0300

    feat: adds tests for counter_ocurrences function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    word_ocurrence = 646
+    assert count_ocurrences('data/jobs.csv', 'Salesforce') == word_ocurrence
